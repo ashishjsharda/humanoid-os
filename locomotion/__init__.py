f60559afade0cedbf7556acc3c840825b9cbf1eb
Ashish Sharda <ashishjsharda@gmail.com>
2025-12-05 00:47:09 -0500

    Add exports for advanced gaits and push recovery modules

--- locomotion/__init__.py
+++ locomotion/__init__.py
@@ -4,13 +4,25 @@ Locomotion systems for bipedal humanoid robots
 
 from .balance import BalanceController, BalanceConfig, BalanceState
 from .gait import GaitGenerator, GaitConfig, GaitPhase, GaitState
+from .advanced_gaits import AdvancedGaitGenerator, AdvancedGaitConfig, GaitType
+from .push_recovery import PushRecoveryController, DisturbanceConfig, DisturbanceInfo
 
 __all__ = [
+    # Balance
     'BalanceController',
     'BalanceConfig',
     'BalanceState',
+    # Basic Gait
     'GaitGenerator',
     'GaitConfig',
     'GaitPhase',
     'GaitState',
+    # Advanced Gaits
+    'AdvancedGaitGenerator',
+    'AdvancedGaitConfig',
+    'GaitType',
+    # Push Recovery
+    'PushRecoveryController',
+    'DisturbanceConfig',
+    'DisturbanceInfo',
 ]
